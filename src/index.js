6d7b2a325b8354cfdc8dc0fca6e979f4d82a0e69
Ipshita Patel <ipshita2908@gmail.com>
2026-09-11 13:40:04 +0530

    feat: add project dependency detection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
-console.log("Snooply is snooping around!");
+const fs = require("fs");
+const path = require("path");
+
+console.log("Snooply is snooping around your project...");
+
+const projectPath = process.cwd();
+const packageJsonPath = path.join(projectPath, "package.json");
+
+if (!fs.existsSync(packageJsonPath)) {
+  console.log("Snooply couldn't find a package.json here.");
+  process.exit(1);
+}
+
+const packageJson = JSON.parse(
+  fs.readFileSync(packageJsonPath, "utf-8")
+);
+
+const dependencies = {
+  ...packageJson.dependencies,
+  ...packageJson.devDependencies,
+};
+
+console.log("\nSnooply found your dependencies:\n");
+
+for (const dependency of Object.keys(dependencies)) {
+  console.log(`• ${dependency}`);
+}
